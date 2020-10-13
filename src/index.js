540c66c0cdd70f760c3f712112d8b84fea6a5bbc
RomanFilipenia <biologg@mail.ru>
2020-10-13 19:42:01 +0300

    new

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (int) {
+    int=int.toString().split('');
+    console.log(int);
+    let arr=[];
+   for(let i=int.length-1; i>=0;i--){
+        arr[int.length-1-i]=int[i];
+   }
+   arr=parseInt(arr.join(''),10);
+    return arr;
+}
 }
